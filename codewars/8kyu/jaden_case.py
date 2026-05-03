98fe99526f5ddac0a6d9389a68c650ac19baa732
brodga81-source <brodga81@gmail.com>
2026-05-03 18:06:32 +0500

    Update jaden_case.py

--- codewars/8kyu/jaden_case.py
+++ codewars/8kyu/jaden_case.py
@@ -3,4 +3,10 @@
 # Link: https://www.codewars.com/kata/5390bac347d09b7da40006f6
 
 def to_jaden_case(string):
-    return ' '.join(word.capitalize() for word in string.split())
+    quot = string.split()
+    empty_lst = []
+    for word in quot:
+        empty_lst.append(word.capitalize())
+
+    return ' '.join(empty_lst)
+
