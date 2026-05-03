e5415604cebd357537832a67ea06cf358f7d03ac
brodga81-source <brodga81@gmail.com>
2026-05-03 17:50:25 +0500

    Create jaden_case.py

---
+++ codewars/8kyu/jaden_case.py
@@ -0,0 +1,6 @@
+# Task: Jaden Casing Strings
+# Source: Codewars
+# Link: https://www.codewars.com/kata/5390bac347d09b7da40006f6
+
+def to_jaden_case(string):
+    return ' '.join(word.capitalize() for word in string.split())
