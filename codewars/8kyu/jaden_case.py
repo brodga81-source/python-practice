dd60f8a687a58d8db2c872a66983e8eea80a674d
brodga81-source <brodga81@gmail.com>
2026-05-03 19:00:01 +0500

    Update jaden_case.py

--- codewars/8kyu/jaden_case.py
+++ codewars/8kyu/jaden_case.py
@@ -2,11 +2,11 @@
 # Source: Codewars
 # Link: https://www.codewars.com/kata/5390bac347d09b7da40006f6
 
-def to_jaden_case(string):
-    quot = string.split()
-    empty_lst = []
-    for word in quot:
-        empty_lst.append(word.capitalize())
-
-    return ' '.join(empty_lst)
-
+def to_jaden_case(string: str) -> str:
+    """
+    Преобразует строку так, чтобы каждое слово начиналось с заглавной буквы.
+    """
+    return ' '.join(word.capitalize() for word in string.split())
+if __name__ == "__main__":
+    example = "how can mirrors be real if our eyes aren't real"
+    print(to_jaden_case(example))
