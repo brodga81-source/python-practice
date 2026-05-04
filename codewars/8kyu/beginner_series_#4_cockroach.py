a0481fe60ab02e7296f9ee0a5e48744af675ad1b
brodga81-source <brodga81@gmail.com>
2026-05-04 20:15:21 +0500

    Create beginner_series_#4_cockroach.py

---
+++ codewars/8kyu/beginner_series_#4_cockroach.py
@@ -0,0 +1,9 @@
+#Beginner Series #4 Cockroach
+#Codewars
+#https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/python
+
+def cockroach_speed(s):
+'''
+Функция переводит км/ч в см/c
+'''
+    return int(s * 100000 / 3600)
