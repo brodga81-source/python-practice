0bb1955143cf6a6aa9558ccf9c240adf135b86b4
Ruslan Sharipov <brodga81@gmail.com>
2026-05-13 13:00:23 +0500

    Create the_feast_of_many_beasts.py

---
+++ codewars/8kyu/the_feast_of_many_beasts.py
@@ -0,0 +1,7 @@
+#The Feast of Many Beasts
+#Codewars
+#https://www.codewars.com/kata/5aa736a455f906981800360d/train/python
+def feast(beast, dish):
+    if beast[0] == dish[0] and beast[-1] == dish[-1]:
+        return True
+    return False
