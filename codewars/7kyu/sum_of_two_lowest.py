8a728443b03eebe77e1ed2843a5e71ec53e4fd22
Ruslan Sharipov <brodga81@gmail.com>
2026-07-09 09:21:56 +0500

    Create sum_of_two_lowest.py

---
+++ codewars/7kyu/sum_of_two_lowest.py
@@ -0,0 +1,6 @@
+# Sum of two lowest
+# Codewars
+# https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
+
+def sum_two_smallest_numbers(numbers):
+    return sorted(numbers)[0] + sorted(numbers)[1]
